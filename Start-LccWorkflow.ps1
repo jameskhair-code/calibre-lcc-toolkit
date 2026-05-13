f428735764e98ad23df9ee88d27859086d783e93
James Keith Hair <253932851+jameskhair-code@users.noreply.github.com>
2026-05-12 21:54:21 -0400

    Bump toolkit to v0.12.6

--- Start-LccWorkflow.ps1
+++ Start-LccWorkflow.ps1
@@ -138,7 +138,7 @@ function Get-DefaultImportPath {
 
 function Show-Header {
     Clear-Host
-    Write-Host "Calibre LCC Toolkit v0.12.5" -ForegroundColor Cyan
+    Write-Host "Calibre LCC Toolkit v0.12.6" -ForegroundColor Cyan
     Write-Host "========================"
     Write-Host ""
     Write-Host "Toolkit root:"
